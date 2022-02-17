f9333d98f89a378e4d6f57bc85a57e133966e6eb
paschovanez <paschovanez@uclan.ac.uk>
2022-02-17 15:55:53 +0200

    -Restructured the Files system to use 2 main CSS/JS files for overall page/navigation, but seperate files for page specific functionality/items looks -Added Quiz Empty Items for initialisation

---
+++ CO1111-TreasureHunt/Quiz/QuizJS.js
@@ -0,0 +1,17 @@
+/*
+    * Implementation of:
+    * - List of Games Available
+    * - Selected Quiz with relevant Questions/Answers
+     (Anything else that will be necessary)
+*/
+
+
+/*
+    *
+    *
+    *
+    * List of all available Games
+    *
+    *
+    *
+*/
